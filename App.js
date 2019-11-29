3ea0118b7652295a2f0ee8361a4b102a573f3df0
@maxgfr <maxgolfier@gmail.com>
2019-11-29 17:38:44 +0100

    push

--- App.js
+++ App.js
@@ -4,21 +4,27 @@ import {
   View,
   Button,
   Linking,
-  Alert
+  Alert,
+  NetInfo
 } from 'react-native';
 import Firebase from './lib/firebase';
 import * as DocumentPicker from 'expo-document-picker';
 import * as mime from 'react-native-mime-types';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
+import * as Progress from 'react-native-progress';
 
 export default function App() {
+
+  state = {
+    progress: 0
+  }
 
   _onPressDocumentPicker = async () => {
     const result = await DocumentPicker.getDocumentAsync({});
     console.log('Result:', result);
     if (result.type != "cancel" && !result.cancelled) {
-      this.addToFirebase(result.uri, result.name)
+      this.addToFirebase(result.uri, result.name, result.size)
     }
   }
 
@@ -26,7 +32,7 @@ export default function App() {
     const result = await DocumentPicker.getDocumentAsync({});
     console.log('Result:', result);
     if (result.type != "cancel" && !result.cancelled) {
-      this.addToFirebase(result.uri, result.name);
+      this.addToFirebase(result.uri, result.name, result.size);
     }
   }
 
@@ -34,7 +40,7 @@ export default function App() {
     await this.pickContent((result) => {
       console.log('Result:', result);
       if (result.type != "cancel" && !result.cancelled) {
-        this.addToFirebase(result.uri, result.name);
+        this.addToFirebase(result.uri, result.name, result.size);
       }
     });
   }
@@ -43,14 +49,15 @@ export default function App() {
     await this.takePicture((result) =>{
       console.log('Result:', result);
       if (result.type != "cancel" && !result.cancelled) {
-        this.addToFirebase(result.uri, result.name);
+        this.addToFirebase(result.uri, result.name, result.size);
       }
     });
   }
 
-  addToFirebase = (uri, name) => {
+  addToFirebase = (uri, name, size) => {
     var extension = uri.split('.').pop();
     var contentType = mime.lookup(extension);
+    this.initProgressBar(size);
     let firebase = Firebase.getInstance();
     if(!name) { name = '' }
     firebase.saveStorage('new/', uri, name, contentType, extension)
@@ -60,6 +67,15 @@ export default function App() {
       .catch((error) => {
           console.log(error);
       });
+  }
+
+  initProgressBar = (size) => {
+    console.log(size);
+    NetInfo.getConnectionInfo().then(connectionInfo => {
+      console.log(
+        'Initial, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType
+      );
+    });
   }
 
   pickContent = async (callback) => {
@@ -122,6 +138,11 @@ export default function App() {
           onPress={this._onPressCamera}
           style={styles.button}
         />
+        {
+          this.state.progress != 0 ?
+          <Progress.Circle progress={this.state.progress} size={75} showsText={true} />
+          : null
+        }
     </View>
   );
 }
